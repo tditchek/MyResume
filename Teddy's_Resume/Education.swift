c92812cd99511146de8519bb1390660196a36e3e
t.ditchek@gmail.com <t.ditchek@gmail.com>
2015-07-24 19:55:36 -0400

    added Education model. added some temporary UI to Education ViewController in storyboard

--- Teddy's_Resume/Education.swift
+++ Teddy's_Resume/Education.swift
@@ -10,8 +10,38 @@ import UIKit
 
 struct Education {
     
+    // TODO: eventually allow multiple colleges and extend to other types of learning institutions
     var college: String
+    // TODO: eventually allow multiple degrees
+    var degree: String
+    var graduationDate: String
+    var majors: [Major]
+    var awards: [Award]
     
+}
+
+struct Major {
+    var name: String
+}
+
+struct Award {
+    var title: String
+    // TODO: make sure year has bounds check
+    var year: Int
+}
+
+enum DegreeType {
+    case BA
+    case BS
+    case PhD
+    case MA
+    case MBA
+}
+
+struct College {
+    var name: String
+    var startedDate: String
+    var graduationDate: String
 }
 
 //
